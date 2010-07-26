cd750d0ef94645de2ba6e1b9b976264af92570e2
Nishibayashi Takuji <takuji@senchan.jp>
2010-07-26 23:29:33 +0900

    設定とか書き換えた

--- webroot/index.php
+++ webroot/index.php
@@ -50,7 +50,7 @@
  *
  */
 	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-         define('CAKE_CORE_INCLUDE_PATH',  DS . 'home' . DS . 'takuji' . DS . 'project' . DS . 'cake');
+         define('CAKE_CORE_INCLUDE_PATH',  dirname(dirname(dirname(__FILE__))) . DS . 'cake');
 	}
 
 /**
