45d099fe9d1e2307ad8862d65550d34f73388a4e
Nishibayashi Takuji <takuji@senchan.jp>
2010-07-30 00:45:47 +0900

    $_SERVER()をenv()に書き換えた

--- app_model.php
+++ app_model.php
@@ -3,7 +3,8 @@
 class AppModel extends Model {
 
     public function __construct($id = false, $table = null, $ds = null){
-        $this->useDbConfig = !empty($_SERVER['ENVIRONMENT']) ? $_SERVER['ENVIRONMENT'] : 'default';
+        $env = env('ENVIRONMENT');
+        $this->useDbConfig = !empty($env) ? env('ENVIRONMENT') : 'default';
         parent::__construct($id, $table, $ds);
     }
 
