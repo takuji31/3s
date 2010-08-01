38c43c907ee8aaedc81981fe4d077f9d7a41e1de
Nishibayashi Takuji <takuji@senchan.jp>
2010-08-02 00:52:49 +0900

    DebigKitの有効化とDB環境切り替えに対応

--- app_controller.php
+++ app_controller.php
@@ -8,12 +8,12 @@
  * PHP versions 4 and 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
  * @package       cake
  * @subpackage    cake.app
@@ -31,4 +31,14 @@
  * @subpackage    cake.app
  */
 class AppController extends Controller {
+    public $menu = false;
+    public $helpers = array('Html','Form','Javascript');
+    public $components = array('Session','DebugKit.Toolbar');
+    public function beforeFilter(){
+        $prefix = @$this->params['prefix'];
+        if($prefix == 'admin'){
+            $this->layout = 'admin';
+        }
+    }
 }
+?>
--- app_model.php
+++ app_model.php
@@ -2,9 +2,11 @@
 
 class AppModel extends Model {
 
+    public $actsAs = array('Senchan.CreateRid');
+
     public function __construct($id = false, $table = null, $ds = null){
         $env = env('ENVIRONMENT');
-        $this->useDbConfig = !empty($env) ? env('ENVIRONMENT') : 'default';
+        $this->useDbConfig = $env == 'pub' ? 'pub' : 'default';
         parent::__construct($id, $table, $ds);
     }
 
